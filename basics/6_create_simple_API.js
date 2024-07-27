1fac4fbbe3be22fcbdd09818403d4daaa9a4b283
Prakash Gurung <prakashgrg4400@gmail.com>
2024-07-27 22:13:17 +0545

    configure prettier in your project

--- basics/6_create_simple_API.js
+++ basics/6_create_simple_API.js
@@ -1,28 +1,28 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const slugify = require("slugify");
-const replaceTemplate = require("./modules/replaceTemplate");
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 //!==> "__dirname" is a node variable which is predefned inside node, and it gives us the perfect location or path of file in which we are currently working. Suppose we are working inside directory named "basics" so using "__dirname" will return us "C:\Desktop\node\basics" . And it applies for any folder.
 //!==> We want to read the file only once, when "/api" request is called at the beginning, after that we dont want to read the file again and again, so we are reading the file outside the callBack function of "server", because "server callback function" will be called again again each time user sends request again and again , and json data will be read again and again.
-const data = fs.readFileSync(`${__dirname}/6_dev_data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/6_dev_data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 //===> Below 2 line code is just to show that how we third party libraries using documentation.
-const slug = dataObj.map(el=>slugify(el.productName , {lower:true}))
+const slug = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slug);
 
 const overviewTemplate = fs.readFileSync(
     `${__dirname}/templates/overview.html`,
-    "utf-8"
+    'utf-8'
 );
 const productTemplate = fs.readFileSync(
     `${__dirname}/templates/product.html`,
-    "utf-8"
+    'utf-8'
 );
 const cardTemplate = fs.readFileSync(
     `${__dirname}/templates/card.html`,
-    "utf-8"
+    'utf-8'
 );
 
 //=================================================================================================================================
@@ -50,46 +50,46 @@ const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
     // const pathname = req.url;
     //!====================================== Overview page ======================================
-    if (pathname === "/" || pathname === "/overview") {
+    if (pathname === '/' || pathname === '/overview') {
         //!==> this cardHTMl variable will store a html data of all the json data, with the help of map.
         const cardHTML = dataObj
             .map((prod) => replaceTemplate(cardTemplate, prod))
-            .join(""); //!==> The join method converts an array into a single string, by joining the data inside array with the argument passed inside "join()" method. In this case we are joining array data with nothing.
+            .join(''); //!==> The join method converts an array into a single string, by joining the data inside array with the argument passed inside "join()" method. In this case we are joining array data with nothing.
 
-        const output = overviewTemplate.replace("{%PRODUCT_CARDS%}", cardHTML); //!Finally after obtaining the cardTemplate of all the json data, we will now use that template inside "overviewTemplate" by replacing "{%PRODUCT_CARDS%}" with "cardHTML" as shown above. As "{%PRODUCT_CARDS%}" is also a template placeholder or dummy data.
+        const output = overviewTemplate.replace('{%PRODUCT_CARDS%}', cardHTML); //!Finally after obtaining the cardTemplate of all the json data, we will now use that template inside "overviewTemplate" by replacing "{%PRODUCT_CARDS%}" with "cardHTML" as shown above. As "{%PRODUCT_CARDS%}" is also a template placeholder or dummy data.
 
         res.writeHead(200, {
-            "Content-type": "text/html",
+            'Content-type': 'text/html',
         }); //!==> Here we are verifying to the user, that request was successful by passing "200" , and the header data, where we are explaining that the response send by the server is  a html file.
         res.end(output);
         //!====================================== Product page ======================================
-    } else if (pathname === "/product") {
+    } else if (pathname === '/product') {
         res.writeHead(200, {
-            "Content-type": "text/html",
+            'Content-type': 'text/html',
         });
         const product = dataObj.find((item) => item.id === Number(query.id)); //!Selecting product whose id is eqal to search parameters.
         const output = replaceTemplate(productTemplate, product); //!replacing placeholders with real data.
         res.end(output); //!displaying data as response to the request.
         //!====================================== API page ======================================
-    } else if (pathname === "/api") {
+    } else if (pathname === '/api') {
         // fs.readFile(`${__dirname}/6_dev_data/data.json`, "utf-8", (err, data) => {
         res.writeHead(200, {
             //!==> 200 status code shows that the response is successfully delieverd without any error.
-            "Content-type": "application/json", //!==> This header data show what type of data will be displayed in the screen as a response i.e. "json" data.
+            'Content-type': 'application/json', //!==> This header data show what type of data will be displayed in the screen as a response i.e. "json" data.
         });
         res.end(data);
         // });
         //!====================================== Error page ======================================
     } else {
         res.writeHead(404, {
-            "Content-type": "text/html",
+            'Content-type': 'text/html',
         });
-        res.end("<h1>Page not found</h1>");
+        res.end('<h1>Page not found</h1>');
     }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-    console.log("Starting the server at port 8000");
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Starting the server at port 8000');
 });
 
 //!==> After completing our small project "node farm" , we deep dived into npm i.e. "node package manager" which is used to manage the packages in our node.
@@ -108,7 +108,6 @@ server.listen(8000, "127.0.0.1", () => {
 //!=============== slugify ================
 //! ==> now here we will implement sulgify i.e. third party library by reading documnetation.
 
-
 //!==================================== deep dive into package version,update and delete ========================
 //!==>  Inside "package.json" you will get to see all the regular and development dependencies in a structure like this:-
 //!                                                       "dependencies": {
@@ -120,3 +119,10 @@ server.listen(8000, "127.0.0.1", () => {
 //! --> "npm install package_name@version" , this will help you install a package of your desired version. For eg:- we have a package named "slugify" . Suppose I dont want the latest version of "slugify" i.e. "1.6.6". than you can write your own version as  "npm install slugify@1.0.0" .
 //! --> "npm update slugify" , It will update you package to the latest version based on *,^,~ .
 //! --> "npm uninstall slugify" , it will uninstall a package.
+
+//!================ Prettier ===========
+//! --> You can configure your prettier setting by adding ".prettierrc" file and configure all you want. Inside ".prettierrc" file
+//!                        {
+//!                               "singleQuote": true,
+//!                               "tabWidth": 4
+//!                         }
