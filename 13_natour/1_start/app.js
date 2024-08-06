417d6787edaf3f7524b98f1e4c7cd1cee793f659
Prakash Gurung <prakashgrg4400@gmail.com>
2024-08-06 15:17:56 +0545

    patch and delete http method done

--- 13_natour/1_start/app.js
+++ 13_natour/1_start/app.js
@@ -87,6 +87,37 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch("/api/v1/tours/:id" , (req , res)=>{
+  if(req.params.id>=tours.length)
+  {
+    return res.status(404).json({
+      status:"fail",
+      message:"Invalid id"
+    })
+  }
+  
+  res.status(200).json({
+    status:"success",
+    data:{
+      tour:"<Your updated tour herer>"
+    }
+  })
+})
+
+app.delete("/api/v1/tours/:id" , (req , res)=>{
+  if((req.params.id*1)>=tours.length)
+  {
+    return res.status(404).json({
+      status:"fail",
+      message:"invalid id" 
+    })
+  }
+  res.status(204).json({
+    status:"success",
+    data:null
+  })
+})
+
 const port = 3000;
 
 app.listen(port, () => {
