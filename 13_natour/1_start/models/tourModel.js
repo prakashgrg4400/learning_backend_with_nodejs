4e7ee25a95cbb7d72e75704046f136fee1d87048
Prakash Gurung <prakashgrg4400@gmail.com>
2024-09-06 21:28:46 +0545

    modelling the schema inside models

--- 13_natour/1_start/models/tourModel.js
+++ 13_natour/1_start/models/tourModel.js
@@ -2,22 +2,58 @@ const mongoose = require('mongoose');
 
 //!====> Creating schema for tours using mongoose .
 const tourSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, 'A tour must have a name'], // first parameter is saying you cannot leave this field empty, second is error message.
-      unique: true,// all names must be unique otherwise , we will get error.
-    },
-    rating: {
-      type: Number,
-      default: 4,
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-    },
-  });
-  
-  //!====> Creating modal for tour
-  const Tour = mongoose.model('Tour', tourSchema);
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // first parameter is saying you cannot leave this field empty, second is error message.
+    unique: true, // all names must be unique otherwise , we will get error.
+    trim:true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required:[true , "A tour must have a summary"]
+  },
+  description:{
+    type:String,
+    trim:true
+  },
+  imageCover:{
+    type:String,
+    required:[true , "Atour must have a cover image"]
+  },
+  images:[String],// storing multiple images--> i.e. name of images in an array
+  createdAt:{
+    type:Date,
+    default:Date.now()
+  },
+  startDates:[Date]
+});
 
-module.exports = Tour ;
+//!====> Creating modal for tour
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
