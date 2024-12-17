c35c6da967af17e978c155a982216eca384f6dd7
Prakash Gurung <prakashgrg4400@gmail.com>
2024-12-17 21:37:59 +0545

    Handeling undefined routes

--- 13_natour/1_start/app.js
+++ 13_natour/1_start/app.js
@@ -34,6 +34,16 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Handeling the undefined routes, which are not defined in our middleware function .
+// The middleware will execute from top to bottom , and if line 34 and 35 are not able to get the routes, entered by user than it is not a valid route and than the middleware below will be executed which will handle all the remaining undefined routes .
+app.all('*', (req, res, next) => {
+  // all ==> It represents all the http request i.e. "get" , "post" , "delete" etc .
+  res.status(404).json({
+    status: 'fail',
+    message: `No such route ${req.originalUrl} is available`,
+  });
+});
+
 module.exports = app;
 
 //!---> Here we structured our file. In above commented codes "middleware" , "router" , "route handler or controller" all of them in a single file "app.js" . But now we have created separate file router i.e. "routes file" where all the router are created , separate file for route handeler or controller i.e. "controllers" where we define all the route handlers and export them . And in app.js we will write our code related to "middleware" . And we even created our server in a separate file i.e. "server.js" .
