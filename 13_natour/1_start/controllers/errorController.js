51138a0ff6edaa0e9363f0edd0f40e905e9b943c
Prakash Gurung <prakashgrg4400@gmail.com>
2024-12-20 09:54:51 +0545

    Handeling error separately for 'development' and 'production'

--- 13_natour/1_start/controllers/errorController.js
+++ 13_natour/1_start/controllers/errorController.js
@@ -1,13 +1,46 @@
 //!===> In a middleware function, if we pass four arguments, than express will automatically understands that its a global error handler middleware. And it can be called by passing an error object argument inside "next()" function
 
+//==> During development phase we need send more details about the errors, so it will be easy for developers to understand the source of error.
+function developmentErrorHandler(err, res) {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+}
+
+//==> During production , the client uses the application, so we need to send more standard error with less description about the error .
+function producitonErrorHandler(err, res) {
+  //==> Checking whether the error is programming error or operational(error occured due to wrong input or data) .
+  if (err.isOperational) {
+    // Handeling error caused by user because of invalid input , routes etc .
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Handeking error caused by programming logic .
+    // log the error , to get more information about the error, without letting the user know about the error .
+    console.error('Error ==> ', err);
+
+    // sending generic message , i.e. standard message .
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+}
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack); // the stack property stores all the information about the error, like what triggered the error and its details.
 
   err.statusCode = err.statusCode || 500; // 500 ==> internal server error
   err.status = err.status || 'Error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    developmentErrorHandler(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    producitonErrorHandler(err, res);
+  }
 };
--- 13_natour/1_start/utils/appError.js
+++ 13_natour/1_start/utils/appError.js
@@ -3,6 +3,7 @@ class AppError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true; // this error property will be used to distinguish the error type i.e. operational(error caused by invalid input)    or   programming(error caused due to programming logic ) .
 
     Error.captureStackTrace(this, this.constructor); // here we are saying that keep track of the error of the current object i.e first argument "this" and the second argument "this.constructor" is to say that exclude the information of the "AppError" constructor in the error details, as it includes unnecessary details like the description of the error, which will make the error information longer.
   }
